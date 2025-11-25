c41e4a52458c1fbda010e82d5ade59006715c732
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-11-25 14:42:07 +0700

    feat: add express swagger-jsdod login and register

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,6 +1,16 @@
 import { craeteUser, findUserByEmail } from "../models/user.models.js";
 
-
+/**
+ * POST /auth/register
+ * @summary login user
+ * @tags Auth
+ * @param {object} request.body.required - Body request
+ * @param  {string} fullname.form.required - fullname of user - application/x-www-form-urlencoded
+ * @param  {string} email.form.required - Email of user - application/x-www-form-urlencoded
+ * @param  {string} password.form.required - Password of user - application/x-www-form-urlencoded
+ * @return {object} 200 - Sukses response
+ * @return {object} 401 - email or password correct
+ */
 export function register(req, res){
     const {fullname, email, password} = req.body
     const newUser = craeteUser(fullname, email, password)
@@ -12,6 +22,17 @@ export function register(req, res){
     })
 }
 
+
+/**
+ * POST /auth/login
+ * @summary login user
+ * @tags Auth
+ * @param {object} request.body.required - Body request
+ * @param  {string} email.form.required - Email of user - application/x-www-form-urlencoded
+ * @param  {string} password.form.required - Password of user - application/x-www-form-urlencoded
+ * @return {object} 200 - Sukses response
+ * @return {object} 401 - email or password correct
+ */
 export function login(req, res) {
   const { email, password } = req.body;
   const user = findUserByEmail(email);
@@ -23,7 +44,7 @@ export function login(req, res) {
       data: user,
     });
   } else {
-    res.json({
+    res.status(401).json({
       success: false,
       message: "Email atau password salah",
     });
