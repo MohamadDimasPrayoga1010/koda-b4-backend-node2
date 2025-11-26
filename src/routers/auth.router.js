197ae266a1e8207da2b71b0a30d41ae10b05d3a0
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-11-26 12:03:48 +0700

    feat add validation auth

--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { register, login } from "../controllers/auth.controllers.js";
+import { loginValidation, resgisterValidation } from "../middlewares/authValidator.js";
+import { validate } from "../middlewares/validation.js";
 
 const router = express.Router();
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register",resgisterValidation,validate ,register);
+router.post("/login", loginValidation, validate, login);
 
 export default router;
