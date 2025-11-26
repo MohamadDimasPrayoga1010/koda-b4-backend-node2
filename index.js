c2652c35e5e56b8f7b7e6727124cfa1fc65c0e57
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-11-26 18:00:57 +0700

    feat: add token to product

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import productRouter from "./src/routers/product.router.js"
 import { initDocs } from "./src/libs/initDocs.js";
 import path from "node:path"
 import 'dotenv/config';
+import { authMiddleware } from "./src/middlewares/authMiddleware.js";
 
 const app = express();
 
@@ -11,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 app.use("/auth", authRoutes);
-app.use("/api", productRouter)
+app.use("/api", authMiddleware, productRouter)
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 initDocs(app)
