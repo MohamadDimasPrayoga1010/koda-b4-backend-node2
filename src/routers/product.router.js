ab6cc93474af64fe68326bbdeb48772ee40342fe
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-11-26 12:04:01 +0700

    feat add validation PRODUCT

--- src/routers/product.router.js
+++ src/routers/product.router.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { products, productById, createProduct, editProduct, removeProduct, uploadProductPicture } from "../controllers/products.controllers.js";
+import { createProductValidation, updateProductValidation } from "../middlewares/productsValidator.js";
+import { validate } from "../middlewares/validation.js";
 
 const router = express.Router();
 
 router.get("/products", products);
 router.get("/products/:id",productById)
-router.post("/products", createProduct)
-router.patch("/products/:id", editProduct);
+router.post("/products",createProductValidation, validate ,createProduct)
+router.patch("/products/:id", updateProductValidation, validate, editProduct);
 router.post("/products/:id/upload", uploadProductPicture);
 router.delete("/products/:id", removeProduct);
 export default router;
